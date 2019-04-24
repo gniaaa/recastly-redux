87c215722a00fedb304ae6ab92f1bdd763ea10c9
gniaaa <eugenia.osy@gmail.com>
2019-04-24 16:21:20 -0700

    Implement reducers

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,15 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  var videos = searchYouTube({ key: YOUTUBE_API_KEY, query: q });
+  dispatch(changeVideoList(videos));
+  dispatch(changeVideo(videos[0]));
+
+  return {
+    type: 'HANDLE_VIDEO_SEARCH',
+    videos: videos
+  };
+
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
-import currentVideo from './currentVideo.js';
-import videoList from './videoList.js';
+import currentVideoReducer from './currentVideo.js';
+import videoListReducer from './videoList.js';
+// import changeVideoList from '../actions/videoList.js';
+// import changeVideo from '../actions/currentVideo.js';
 
-var rootReducer = () => {};
+var rootReducer = combineReducers({
+  videoList: videoListReducer,
+  currentVideo: currentVideoReducer
+});
 
 //TODO: define the root reducer for this app
 
